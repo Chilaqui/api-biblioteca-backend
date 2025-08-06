10df13d2b39a6af64dc47c4a914f4bff1ccfd761
Hector <comhector_@hotmail.com>
2025-08-06 16:41:52 -0600

    Creando la clase Book

---
+++ app/src/main/java/com/library/app/model/book.java
@@ -0,0 +1,50 @@
+package com.library.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class book {
+
+    private Long id;
+    private String name;
+    private String author;
+    private String description;
+    private int stock;
+    
+    public book() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+}
