2818062bbe728bb53f69f52b4a3d40b9f86a5f33
Hector <comhector_@hotmail.com>
2025-08-13 00:29:39 -0600

    Configurando autor con los datos jpa

--- app/src/main/java/com/library/app/model/Author.java
+++ app/src/main/java/com/library/app/model/Author.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,11 +18,14 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "name", length = 50, nullable = false, unique = true )
     private String name;
-    @Column(name = "country", length = 50, nullable = false, unique = true)
+
+    @Column(name = "country", length = 50, nullable = false)
     private String country;
-    @OneToMany()
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
     private List<Book> books;
     
     public Author() {
