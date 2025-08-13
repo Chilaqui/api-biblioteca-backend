0064af9d8a6bb7b5907badd3f8fcabf8a8062c4c
Hector <comhector_@hotmail.com>
2025-08-13 01:47:14 -0600

    Creacion de loan, para los prestamos que se tienen que modificar con sus respectivas configuraciones de vilculacion

---
+++ app/src/main/java/com/library/app/model/Loan.java
@@ -0,0 +1,82 @@
+package com.library.app.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "loan")
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "library_user_id")
+    private LibraryUser libraryUser;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @NotNull
+    @Column(name = "loan_date", nullable = false, unique = false)
+    private LocalDateTime loanDate;
+
+    @NotNull
+    @Column(name = "return_date", nullable = false, unique = false)
+    private LocalDateTime returnDate;
+
+    //Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LibraryUser getLibraryUser() {
+        return libraryUser;
+    }
+
+    public void setLibraryUser(LibraryUser libraryUser) {
+        this.libraryUser = libraryUser;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDateTime loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    
+}
